bf0c3b40bbd55b31b5721c9362a60d0af299537e
WG <wgrandoms@gmail.com>
2021-04-04 13:41:55 -0400

    Added code to display posts on the index page

--- app/routes.py
+++ app/routes.py
@@ -7,5 +7,15 @@ def index():
     """This function implements what the index page displays."""
 
     user = {'username': 'Peipei'}
+    posts = [
+        {
+            'username': 'Baiber',
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'username': 'Peipei',
+            'body': 'The new movie on Netflix is great!'
+        }
+    ]
 
-    return render_template('index.html', title='Home', user=user)
+    return render_template('index.html', title='Home', user=user, posts=posts)
