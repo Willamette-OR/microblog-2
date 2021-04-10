4a426e81d7b5ef2e61f3f1faef91c7bb4bd940e0
WG <wgrandoms@gmail.com>
2021-04-10 13:26:45 -0400

    Added a table for posts to the data model and updated db migration scripts

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from app import db 
 
 
@@ -8,6 +9,16 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(128), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return "<User: {}>".format(self.username)
+
+
+class Post(db.Model):
+    """This class implements the database model for posts, derived from the parent class of db.Model."""
+
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    datetime = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
