9bbce01ea845f50b53ebe48718ea667da1e41696
WG <wgrandoms@gmail.com>
2021-04-11 14:34:47 -0400

    Added logic to log out users and render properly too

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, flash, redirect, url_for
-from flask_login import current_user, login_user
+from flask_login import current_user, login_user, logout_user
 from app import app
 from app.form import LoginForm
 from app.models import User
@@ -67,3 +67,11 @@ def login():
         return redirect(url_for('index'))
     
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    """This function logs out users."""
+
+    logout_user()
+    return redirect(url_for('index'))
+
