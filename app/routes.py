bcc0d18622757e3a4b70060fbcce5f5c9da61d67
WG <wgrandoms@gmail.com>
2021-04-12 22:23:27 -0400

    Updated logic to require login before getting to the index page

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,13 @@
-from flask import render_template, flash, redirect, url_for
-from flask_login import current_user, login_user, logout_user
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
 from app import app
 from app.form import LoginForm
 from app.models import User
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     """This function implements what the index page displays."""
 
@@ -64,7 +66,10 @@ def login():
             # The url_for() is also able to generate these complex URLs.
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            return redirect(url_for('index'))
+        return redirect(next_page)
     
     return render_template('login.html', title='Sign In', form=form)
 
