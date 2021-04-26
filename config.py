a371783bb5e886da3244b6b5e2de7ab09710867e
WG <wgrandoms@gmail.com>
2021-04-25 22:16:50 -0400

    Initialized code to enable sending errors by email

--- config.py
+++ config.py
@@ -9,3 +9,10 @@ class Config:
     # The SQLALCHEMY_TRACK_MODIFICATIONS configuration option is set to False to disable a feature of Flask-SQLAlchemy that I do not need, 
     # which is to signal the application every time a change is about to be made in the database.
     SQLALCHEMY_TRACK_MODIFICATIONS = False 
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None 
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['your-email@example.com']
+    
